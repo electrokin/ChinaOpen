4303b5f0d36c4fa10d0e1409959a5f13c41e7ead
electrokin <51311689+electrokin@users.noreply.github.com>
2019-06-30 09:15:38 +0100

    Update EventDetails.js
    updated for ECO

--- north-china-open/src/EventDetails.js
+++ north-china-open/src/EventDetails.js
@@ -25,7 +25,7 @@ class EventDetails extends Component {
 
           <div className="PageHeader">
             <h1>EVENT DETAILS</h1>
-            <h2>Everything you need to know about the NCO</h2>
+            <h2>Everything you need to know about the ECO</h2>
             <p>For full details on the tournament format, translation support, itinerary, prizes, and venue, read on.</p>
           </div>      
 
@@ -36,7 +36,7 @@ class EventDetails extends Component {
             <div className="SectionCopy">
               <h1>EVENT FORMAT</h1>
               <h2>A.K.A. THE RULES</h2>
-              <p>The North China Open is an ITC sanctioned Warhammer 40K Tournament held on the weekend of the 18th - 19th of May in Tianjin, China.</p>
+              <p>The East China Open is an ITC sanctioned Warhammer 40K Tournament held on the weekend of the 14th - 15th of September in Shanghai, China.</p>
               <p>The event will consist of five rounds held over two days, using Frontline Gaming's <a href="https://docs.google.com/document/d/1bUs0HrJ3f6YzR6mWlT1LRLq0i9_0ekf7ah9WhCTxsIo/edit?usp=sharing">ITC Rules</a> and ITC <a href="https://docs.google.com/document/d/18u16xnoStYHxTUL0Eodjk3kTR4MCn9zyF1r3sZkOgfo/edit">Simplified Missions</a>. Players will have up to 3 hours to complete a single mission.</p>
               <p>We willl be using the Matched Play Recommendations from the Warhammer 40,000 8th Edition ruleset, which can be found in the main <a href="https://www.games-workshop.com/en-US/Warhammer-40000-rulebook-eng-2017">Rulebook</a>.</p>
               <p><strong>ITC standard games will be played at 2000 points.</strong></p>
@@ -50,18 +50,23 @@ class EventDetails extends Component {
             </div>
             <div className="SectionSpare">
               <h1>Saturday</h1>
-              <p><strong>8:00-8:30</strong> Player's Briefing</p>
+              <p><strong>8:00-8:15</strong> Player's Briefing</p>
+              <p><strong>8:15-8:30</strong> Round 1 pregame</p>
               <p><strong>8:30-11:30</strong> Round 1</p>
-              <p><strong>11:30-12:15</strong> Lunch (Complementary)</p>
-              <p><strong>12:15-3:15</strong> Round 2</p>
-              <p><strong>3:30-6:30</strong> Round 3</p>
+              <p><strong>11:30-12:00</strong> Lunch</p>
+              <p><strong>12:00-12:15</strong> Round 2 pregame</p>
+              <p><strong>12:15-15:15</strong> Round 2</p>
+              <p><strong>15:15-15:30</strong> Round 3 pregame</p>
+              <p><strong>15:30-18:30</strong> Round 3</p>
             </div>
             <div className="SectionSpare">
               <h1>Sunday</h1>
-              <p><strong>8:30-11:30</strong> Round 4</p>
-              <p><strong>11:30-12:15</strong> Lunch (Complementary)</p>
-              <p><strong>12:15-3:15</strong> Final Round</p>
-              <p><strong>3:15-4:00</strong> Wrap up and Prizes</p>
+              <p><strong>8:00-8:15</strong> Round 4 pregame</p>
+              <p><strong>8:15-11:15</strong> Round 4</p>
+              <p><strong>11:15-11:45</strong> Break </p>
+              <p><strong>11:45-12:00</strong> Round 5 pregame</p>
+              <p><strong>12:00-15:00</strong> Round 5 </p>
+              <p><strong>15:00-15:30</strong> Wrap up and Prizes</p>
             </div>
 
           </div>
@@ -72,10 +77,9 @@ class EventDetails extends Component {
             <div className="SectionCopy">
               <h1>VENUE</h1>
               <h2>The Field of Battle</h2>
-              <p>The Event will be held at TEDA International School in TianJin's Binhai District.</p>
-              <p>The quickest way to the venue from Beijing or Shanghai is to take the C2581 Bullet Train to Binhai station. Be careful though, because not every bullet train to Tianjin stops here. If you find yourself in TianJin South Railway Station due to availability, you can grab a taxi or take the subway to TEDA in Binhai.</p>
+              <p>The Event will be held at Shanghai Minhang District Vanke Bilingual School .</p>
               <p>There will no doubt be players meeting up on route, so join the National Tournaments WeChat group via the QR Code to make travel arrangements and get to know your fellow attendees.</p>
-              <p>We have tentatively set the player cap for this venue at <strong>40 players</strong>, so register now on the BCP Player App to avoid disappointment.</p>
+              <p>We have set the player cap for this venue at <strong>40 players</strong>, so register now on the BCP Player App to avoid disappointment.</p>
             </div>
           </div>
 
