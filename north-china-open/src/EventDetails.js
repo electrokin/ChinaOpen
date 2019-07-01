caa3fc93364b9cae905975c04c142067e338fb17
electrokin <51311689+electrokin@users.noreply.github.com>
2019-07-01 19:40:57 +0100

    Update EventDetails.js

--- north-china-open/src/EventDetails.js
+++ north-china-open/src/EventDetails.js
@@ -42,7 +42,7 @@ class EventDetails extends Component {
               <p><strong>ITC standard games will be played at 2000 points.</strong></p>
               <p>Full army composition rules can be found <a href="/army">here</a>.</p>
               <br />
-              <p><strong>Download the full ITC rules pack for this event <a href={ECO champs missions v2.pdf} download>here</a>.</strong></p>
+              <p><strong>Download the full ITC rules pack for this event <a href={ECOmissions.pdf} download>here</a>.</strong></p>
             </div>
             
             <div className="SectionCopy">
