6af0d5f264d9e5602139d1a2eb0093283e58b8a5
electrokin <51311689+electrokin@users.noreply.github.com>
2019-07-01 14:58:35 +0100

    Update ArmyDetails.js

--- north-china-open-CN/src/ArmyDetails.js
+++ north-china-open-CN/src/ArmyDetails.js
@@ -15,7 +15,7 @@ class ArmyDetails extends Component {
     return (
       <div>
       
-        <div className="CNEN"><p>CN/EN: Looking for this page in English? Check out the English site <a href="https://www.northchinaopen.com">here</a>.</p></div>
+        <div className="CNEN"><p>CN/EN: Looking for this page in English? Check out the English site <a href="https://www.chinawarhammer.com">here</a>.</p></div>
 
         <a href="/" className="NCOLogo"> </a>
 
@@ -34,7 +34,7 @@ class ArmyDetails extends Component {
           <div className="SectionWrapper">
             <div className="SectionCopy">
               <h1>基本要求</h1>
-              <p>华北公开赛的5轮比赛均使用2000分部队进行比赛，遵循战锤40K官方第8版规则中对战模式（Matched Play）规则，以及Frontline Gaming编写的ITC赛制规则。</p>
+              <p>华东公开赛的5轮比赛均使用2000分部队进行比赛，遵循战锤40K官方第8版规则中对战模式（Matched Play）规则，以及Frontline Gaming编写的ITC赛制规则。</p>
               <ul><h2>概述</h2>
                 <li>每位选手所用军表的分数限制为2000分。一分也不能超！</li>
                 <li>包含至多3个分队</li>
@@ -43,7 +43,7 @@ class ArmyDetails extends Component {
               </ul>
               <p>请注意，赛事每一轮的时间限制为3小时。裁判将尽力确保比赛进程能够顺利推进。</p>
               <p>请保持比赛礼仪，并选用你能够在合理的时间内操纵的部队参赛。没有人想看着你移动300个屁精并把时间耗完</p>
-              <p>请务必于5月4日前在BCP app上上传你的军表</p>
+              <p>请务必于9月1日前在BCP app上上传你的军表</p>
               <p><strong>提示：请先查阅 <a href="/details">ITC</a>任务规则，并在组军时为任务优化你的军表。</strong></p>
               <p>在比赛前，请确认必要的选项均已在军表中标识出来，包括但不限于主将和主将特性、武器选择、分队特性、阵营关键字、指挥点数量等选项。</p>
               <p>对规则有任何不清楚之处，请联系裁判和双语赛事运行人员。</p>
@@ -67,7 +67,7 @@ class ArmyDetails extends Component {
                 <li>白矮人杂志（White Dwarf）和官方社区（Warhammer Community）登载的可用于对战模式（Matched Play）的规则；</li>
                 <li>所有最新版的官方勘误和FAQ。</li>
               </ul>
-              <p>规则变动截止日期为比赛开始前一个月。万一4月30号真的出了太空矮人规则书，在本赛事也是用不了的。</p>
+              <p>规则变动截止日期为比赛开始前一个月。万一8月14号真的出了太空矮人规则书，在本赛事也是用不了的。</p>
               <p>还得提一句，仅有战锤40K规则下的部队可用于此赛事。30K或者其他游戏项目的都不行。</p>
             </div>
           </div>
