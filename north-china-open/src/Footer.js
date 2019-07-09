c48fa2476afadbad04d3141650a4127b7fb204d6
electrokin <51311689+electrokin@users.noreply.github.com>
2019-07-09 20:01:05 +0100

    Update Footer.js
    added SOGOFUN logo

--- north-china-open/src/Footer.js
+++ north-china-open/src/Footer.js
@@ -13,6 +13,7 @@ class Footer extends Component {
           <a href="https://www.frontlinegaming.org/community/frontline-gamings-independent-tournament-circuit/" className="Sponsor" id="itc-logo"> </a>
           <a href="https://www.gamemat.eu/" className="Sponsor" id="gamemat-logo"> </a>
           <a href="http://www.tablewar.com/" className="Sponsor" id="tablewar-logo"> </a>
+          <a href="http://www.tablewar.com/" className="Sponsor" id="SOGOFUNlogo"> </a>
         </div>
         <p className="CopyrightNotice">&copy; ITC North China Open 2019 - Web Design by Addam Martin Digital</p>
 
